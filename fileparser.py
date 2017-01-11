0471c8fc84da9fe06f097aa3eb2f29c184084a6d
Kevin Hernon <khernon1@gmail.com>
2017-01-10 22:30:13 -0500

    updates to functions

--- fileparser.py
+++ fileparser.py
@@ -1,13 +1,9 @@
 import string
-from string import strip, rstrip
+from string import strip
 import collections
 import pdb
 import plotly.plotly as py
 import plotly.graph_objs as go
-
-# break down into multiple formulas with __init__
-# add legend to chart
-# break it down into percentages and compare to other documents?
 
 class ParseFiles():
 
@@ -28,12 +24,13 @@ class ParseFiles():
     merged = ''.join(lines_no_breaks).lower().translate(None, string.punctuation)
     # get sorted, unique values in text without spaces
     uniq_merged = sorted(set(merged.translate(None, string.whitespace)))
-    # create dict of letter and count
     
+    # create dictionary of letter and count for each character   
     for char in uniq_merged:
       self.resume_char_dict[char] = merged.count(char)
 
-  def declaration(self):    
+  def declaration(self):
+    # same as resume above
     myfile = open("declaration.txt", "r")
     all_lines = myfile.readlines()
     lines_no_breaks = map(strip, all_lines)
@@ -45,12 +42,19 @@ class ParseFiles():
 
   def compare_and_build_charts(self):
     # bar chart for just the resume
-    resume_data = [go.Bar(
+    data = [go.Bar(
           x=self.resume_char_dict.keys(),
           y=self.resume_char_dict.values(),
-          name='Resume'
+          name='Resume',
+          marker=dict(
+            color='rgb(55, 83, 109)' 
+          )
     )]
-    # py.plot(resume_data, filename='resume-bar')
+    layout = go.Layout(
+      title='Resume Breakdown by Character',
+    )
+    fig = go.Figure(data=data, layout=layout)
+    py.plot(fig, filename='resume-bar')
     
     # add up the count in the values of each dict
     # iterate through and get %
@@ -68,22 +72,29 @@ class ParseFiles():
     resume_data = go.Bar(
           x=resume_percents.keys(),
           y=resume_percents.values(),
-          name='Resume (%)'
+          name='Resume (%)',
+          marker=dict(
+            color='rgb(55, 83, 109)'
+          )
     )
 
     dec_data = go.Bar(
           x=dec_percents.keys(),
           y=dec_percents.values(),
-          name='Declaration (%)'
+          name='Declaration (%)',
+          marker=dict(
+            color='rgb(26, 118, 255)'
+          )
     )
 
     data = [resume_data, dec_data]
-    layout = go.Layout(
+    layout = go.Layout(     
+          title = 'Resume vs Declaration of Independence Breakdown by Character',
           barmode='group'
     )
 
     fig = go.Figure(data=data, layout=layout)
-    py.plot(fig, filename='stacked-bar')
+    py.plot(fig, filename='grouped-bar')
 
 
 ParseFiles()
